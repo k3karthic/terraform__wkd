e221b6947af81f84b615b02b9d4fb03ec1a2b542
Karthic Kumaran Krisnakumar <k3.karthic@protonmail.ch>
2021-05-28 07:58:09 +0000

    reduce ttl to 3600

--- main.tf
+++ main.tf
@@ -96,9 +96,9 @@ resource "aws_cloudfront_distribution" "wkd" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 86400
-    default_ttl            = 86400
-    max_ttl                = 86400
+    min_ttl                = 3600
+    default_ttl            = 3600
+    max_ttl                = 3600
   }
 
   restrictions {
